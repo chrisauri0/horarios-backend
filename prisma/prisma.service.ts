f366b582e6a254677459fe007a874a890e6734da
chrisauri0 <chaparitoveb20@gmail.com>
2025-10-23 18:42:45 -0600

    ft:fix connection database

---
+++ prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { INestApplication, Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  /**
+   * Llamar desde main.ts para cerrar correctamente Nest cuando Prisma detecte beforeExit.
+   * Uso: await prismaService.enableShutdownHooks(app)
+   */
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
