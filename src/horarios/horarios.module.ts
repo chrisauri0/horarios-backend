b2314014ca28e0479923681fc6e280cc92a4cdcb
chrisauri0 <chaparitoveb20@gmail.com>
2025-10-27 17:50:20 -0600

    ft:add a service for python

--- src/horarios/horarios.module.ts
+++ src/horarios/horarios.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HorariosService } from './horarios.service';
 import { HorariosController } from './horarios.controller';
+import { PythonService } from 'src/python/python.service';
+
 
 @Module({
   controllers: [HorariosController],
-  providers: [HorariosService],
+  providers: [HorariosService, PythonService],
 })
 export class HorariosModule {}
